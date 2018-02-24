d07665fccf8d7a3ff933d252eb7b27901139310e
Anthony Clark <anthonyclark.ac@gmail.com>
2018-02-24 01:38:39 -0500

    It works! Basic TwiML output

--- app.rb
+++ app.rb
@@ -1,5 +1,16 @@
 require 'sinatra'
+require 'twilio-ruby'
+
+require 'sinatra/reloader' if development?
+require 'pry' if development?
+require 'pry-byebug' if development?
 
 get '/' do
-  'Hello, World!'
+  response = Twilio::TwiML::VoiceResponse.new do |r|
+    r.say 'This is a test!'
+  end
+
+  logger.info response.to_s
+
+  response.to_xml
 end
